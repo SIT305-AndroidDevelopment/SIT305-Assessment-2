1e6ae593f5c6d7bc18f674e8448a4dfc27e538bc
SHUAIJUN CHEN <chenshua@deakin.edu.au>
2020-05-22 11:27:41 +0800

    Basic Framework of the Assessment Project

--- app/src/main/java/com/example/assessment_2/MyPagerAdapter.java
+++ app/src/main/java/com/example/assessment_2/MyPagerAdapter.java
@@ -3,39 +3,69 @@ package com.example.assessment_2;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class MyPagerAdapter extends PagerAdapter {
+class ContentAdapter extends PagerAdapter {
 
-    private List<View> viewList = new ArrayList<View>();
+    private List<View> views;
 
-    public MyPagerAdapter(List<View> viewList) {
-        this.viewList = viewList;
-    }
-
-    @NonNull
-    @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        container.addView(viewList.get(position));
-        return viewList.get(position);
-    }
-
-    @Override
-    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView(viewList.get(position));
+    public ContentAdapter(List<View> views) {
+        this.views = views;
     }
 
     @Override
     public int getCount() {
-        return viewList.size();
+        return views.size();
     }
 
     @Override
-    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return view == object;
+    public boolean isViewFromObject(View arg0, Object arg1) {
+        return arg0 == arg1;
     }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = views.get(position);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(views.get(position));
+    }
+
 }
+
+//public class MyPagerAdapter extends PagerAdapter {
+//
+//    private List<View> viewList = new ArrayList<View>();
+//
+//    public MyPagerAdapter(List<View> viewList) {
+//        this.viewList = viewList;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+//        container.addView(viewList.get(position));
+//        return viewList.get(position);
+//    }
+//
+//    @Override
+//    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+//        container.removeView(viewList.get(position));
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return viewList.size();
+//    }
+//
+//    @Override
+//    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+//        return view == object;
+//    }
+//}
