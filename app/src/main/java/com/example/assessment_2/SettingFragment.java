66ef5dfad22a9b8be9d8a1c0e541e82626e29208
XIAOFENG <yinxiaof@deakin.edu.au>
2020-05-29 23:09:38 +0800

    keep updating User_page and create a new page called Setting in user page

---
+++ app/src/main/java/com/example/assessment_2/SettingFragment.java
@@ -0,0 +1,73 @@
+package com.example.assessment_2.UI;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.assessment_2.R;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
+
+public class SettingFragment extends Fragment {
+    View view;
+    TextView title;
+    EditText name;
+    EditText pass;
+    TextView mToast;
+    FragmentActivity mContext;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.mContext = getActivity();
+        // this.mToast = Toast.makeText();
+        name = (EditText) inflater.inflate(R.layout.page_login,container,false);
+        pass = (EditText) inflater.inflate(R.layout.page_login,container,false);
+//
+//
+//        title = view.findViewById(R.id.txt_home_title);
+        //title.setText("我是" + getResources().getString(R.string.txt_nav_home));
+
+//生成json
+//        List<StoreBean> storeBeans = new ArrayList<>();
+//        for (int i = 0; i < 3; i++) {
+//            StoreBean storeBean = new StoreBean();
+//            storeBean.setLatitude(1.1);
+//            storeBean.setLongtitude(2.2);
+//            storeBean.setName("3");
+//            storeBeans.add(storeBean);
+//        }
+////
+//        Gson gson = new Gson();
+//        String toJson = gson.toJson(storeBeans);
+//
+        return view;
+    }
+
+    public void check(View v){
+        String uname = "Chase";
+        String upass = "1234";
+        String user = name.getText().toString().trim();
+        String pwd = pass.getText().toString().trim();
+        if(user.equals(uname)&&pwd.equals(upass))
+        {
+            makeText(mContext,"successfully log in", LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(mContext,"sorry, wrong username or password", LENGTH_SHORT).show();
+        }
+
+    }
+    public void Load() {
+        //title.setText("当前时间：\n" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+
+    }
+
+}
